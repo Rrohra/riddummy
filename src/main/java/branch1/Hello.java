7237804d88f3bda80b6401012567f52fd5d30873
rohit rohra <rrrohra200@gmail.com>
2023-01-07 14:12:40 +0530

    Revert "branch2 commit1"
    This reverts commit 3fcde804ef9ac9bc07e699f8209de58658907c86.

--- src/main/java/branch1/Hello.java
+++ src/main/java/branch1/Hello.java
@@ -5,7 +5,4 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("this is branch1 commit 1");
 	}
-	public void function2() {
-		System.out.println("this is branch2 commit 1");
-	}
 }
