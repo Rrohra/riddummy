1a9dc3bf41265a29ef9e8a8330808b7ae55f67bd
rohit rohra <rrrohra200@gmail.com>
2023-01-07 13:42:36 +0530

    this is branch2 commit 2

--- src/main/java/branch1/Hello.java
+++ src/main/java/branch1/Hello.java
@@ -8,4 +8,7 @@ public class Hello {
 	public void function2() {
 		System.out.println("this is branch2 commit 1");
 	}
+	public void function3() {
+		System.out.println("this is branch2 commit2");
+	}
 }
